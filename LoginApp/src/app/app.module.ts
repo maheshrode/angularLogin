c772691d4399069e02dc4633972ada28a7e29b13
ritujakad <drituja@agsft.com>
2018-11-05 16:37:25 +0530

    added login component

--- LoginApp/src/app/app.module.ts
+++ LoginApp/src/app/app.module.ts
@@ -1,15 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
+import { RouterModule, Routes } from '@angular/router';
+import { LoginComponentComponent } from './login-component/login-component.component';
+import { WelcomeComponentComponent } from './welcome-component/welcome-component.component';
+
+const appRoutes: Routes = [
+  { path: '', component: LoginComponentComponent },
+  { path: 'welcome-component', component: WelcomeComponentComponent },
+
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponentComponent,
+    WelcomeComponentComponent
   ],
   imports: [
-    BrowserModule
-  ],
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)],
+
   providers: [],
   bootstrap: [AppComponent]
 })
---
+++ LoginApp/src/app/login-component/login-component.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login-component',
+  templateUrl: './login-component.component.html',
+  styleUrls: ['./login-component.component.css']
+})
+export class LoginComponentComponent implements OnInit {
+
+  constructor( private router:Router) { }
+
+  ngOnInit() {
+  }
+  onLoadLogin(){
+    this.router.navigate(['/welcome-component'])
+  }
+}
